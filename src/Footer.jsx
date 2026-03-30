176af654b1177f8abb3817814ea8873c357e7959
Rohan-Shridhar <rohansm668@gmail.com>
2026-03-30 23:51:08 +0530

    Added icon and link to portfolio

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="footer-cont">
-            <p>Made with ❤️ by Rohan Shridhar</p>
+            <p>Made with <i className="fa-regular fa-heart"></i> by <a href="https://rohan-shridhar.github.io/portfolio">Rohan Shridhar</a></p>
         </div>
     );
 }

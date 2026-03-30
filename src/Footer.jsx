76a7f0ba54241932800991ce60a960490f524464
Rohan-Shridhar <rohansm668@gmail.com>
2026-03-31 00:06:42 +0530

    Included react in footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="footer-cont">
-            <p>Made with <i className="fa-regular fa-heart"></i> by <a href="https://rohan-shridhar.github.io/portfolio">Rohan Shridhar</a></p>
+            <p>Built with <i class="fa-brands fa-react"></i> by <a href="https://rohan-shridhar.github.io/portfolio">Rohan Shridhar</a></p>
         </div>
     );
 }
